ccef4dcace5afb7c1dca37e5047ecdfd4711ddd1
Muchiri <llwmuchiri@gmail.com>
2026-06-05 09:52:28 +0300

    Mailer, changed email

--- Backend/Utils/Mailer.ts
+++ Backend/Utils/Mailer.ts
@@ -5,7 +5,7 @@ const resend = new Resend(RESEND_API_KEY);
 
 export const sendOtpEmail = async (to: string, code: string): Promise<void> => {
   await resend.emails.send({
-    from: "FreshMarikiti <tech@ferracorp.com>",
+    from: "FreshMarikiti <no-reply@ferracorp.com>",
     to,
     subject: "Your Password Reset Code",
     text: `Your OTP code is: ${code}. It expires in 10 minutes.`,
